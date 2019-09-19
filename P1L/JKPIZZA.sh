fe544382176ed2655cda95ab649cfcbfef53d53d
Kathleen <KathleenP89@gmail.com>
2019-09-19 12:16:49 -0400

    added P1L

---
+++ P1L/JKPIZZA.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+clear
+
+echo " *************** W E L C O M E    T O    J K   P I Z Z A***************"
+echo 
+echo 
+echo 
+echo 
+	 	echo "what type of pizza would you like to enjoy"
+
+
+
+Select option in "Cheese" "Veggies" "Meat Lovers" ; do
+
+	echo "The chosen option is $REPLY"
+	echo "The chosen option is $option"
